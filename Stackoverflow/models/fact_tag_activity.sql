24eb583f96e4c8215bccad31d8d26839573d66e5
Eniya Kulshreshtha <ekulshre@g.syr.edu>
2025-05-05 03:39:34 +0000

    commit

--- Stackoverflow/models/fact_tag_activity.sql
+++ Stackoverflow/models/fact_tag_activity.sql
@@ -1,10 +1,7 @@
 with stg_posts as (
     select
         _Id as PostId,
-        {{ dbt_utils.generate_surrogate_key(['_OwnerUserId']) }} as userkey,
-        {{ dbt_utils.generate_surrogate_key(['_Tags']) }} as tagkey,
-        -- Directly use _CreationDate as tagactivitytimestamp
-        _CreationDate as tagactivitytimestamp,
+        _OwnerUserId as userkey,
         _Title as title,
         _ViewCount as viewcount,
         _Score as score,
@@ -27,13 +24,8 @@ stg_tags as (
     from {{ source('stackoverflow', 'tags') }}
 )
 select  
-    {{ dbt_utils.generate_surrogate_key(['stg_posts.PostId', 'stg_tags.tag_id']) }} as tagactivitykey,
     stg_posts.PostId as postkey,
     stg_users.user_id as userkey,
-    stg_tags.tag_id as tagkey,
-    stg_posts.tagactivitytimestamp,
-    stg_tags.tagname,
-    stg_tags.tagcount,
     stg_posts.title,
     stg_posts.viewcount,
     stg_posts.score,
@@ -42,5 +34,4 @@ select
     stg_users.userreputation,
     stg_users.location
 from stg_posts
-    join stg_tags on stg_posts.tagkey = stg_tags.tag_id
     join stg_users on stg_posts.userkey = stg_users.user_id
